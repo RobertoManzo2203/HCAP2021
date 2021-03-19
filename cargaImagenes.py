f6275dda3ee0fd7470bf728dbdbaa68c9bd384f7
RobertoManzoA01748301 <58737561+RobertoManzoA01748301@users.noreply.github.com>
2021-03-19 08:52:16 -0600

    cambios nuevos

--- cargaImagenes.py
+++ cargaImagenes.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
 
-#cargar la imagen a color
-IRGB = cv2.imread('r2d2.jpg')
+#Cargar la imagen a color
+IRGB=cv2.imread('004.jpg')
 print(IRGB)
 print(IRGB.shape)
-
-
+print("Lineas agregadas en la rama2")
+IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
+print(IGS)
+print(IGS.shape)
+cv2.imwrite('004GS.jpg',IGS)
